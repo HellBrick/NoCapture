aa0c38408fff07c32a246485f873d67aed19cea0
HellBrick <hellbrick@outlook.com>
2018-03-18 15:47:32 +0300

    Added SemanticModel.GetSingleSymbol() extension

---
+++ HellBrick.NoCapture.Analyzer/SemanticModel.GetSingleSymbol.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HellBrick.NoCapture.Analyzer
+{
+	internal static partial class SemanticModelExtensions
+	{
+		public static ISymbol GetSingleSymbol( this SemanticModel semanticModel, ExpressionSyntax expression )
+		{
+			SymbolInfo symbolInfo = semanticModel.GetSymbolInfo( expression );
+			return symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.OnlyOrDefault();
+		}
+	}
+}

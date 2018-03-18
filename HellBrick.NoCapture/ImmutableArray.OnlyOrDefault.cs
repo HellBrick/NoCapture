44339cbf63557dbe7796257cf91b8cd2c6b474c5
HellBrick <hellbrick@outlook.com>
2018-03-18 16:56:57 +0300

    Made internal method truly internal

--- HellBrick.NoCapture/ImmutableArray.OnlyOrDefault.cs
+++ HellBrick.NoCapture/ImmutableArray.OnlyOrDefault.cs
@@ -2,7 +2,7 @@
 
 namespace HellBrick.NoCapture.Analyzer
 {
-	public static partial class ImmutableArrayExtensions
+	internal static partial class ImmutableArrayExtensions
 	{
 		public static T OnlyOrDefault<T>( this ImmutableArray<T> array )
 			=> array.Length == 1

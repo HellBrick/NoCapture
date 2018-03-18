4adc82fe1fd1bb293f3a4807fcd6ea119cd2c305
HellBrick <hellbrick@outlook.com>
2018-03-18 18:29:24 +0300

    Added ImmutableArray.IntersectWith

---
+++ HellBrick.NoCapture/Analyzer/ImmutableArray.IntersectWith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace HellBrick.NoCapture.Analyzer
+{
+	internal static partial class ImmutableArrayExtensions
+	{
+		public static ImmutableArray<T> IntersectWith<T>( this ImmutableArray<T> array, ImmutableArray<T> otherArray )
+		{
+			return
+				array.IsEmpty || otherArray.IsEmpty
+				? ImmutableArray<T>.Empty
+				: IntersectSlow();
+
+			ImmutableArray<T> IntersectSlow()
+			{
+				ImmutableArray<T>.Builder builder = ImmutableArray.CreateBuilder<T>( Math.Min( array.Length, otherArray.Length ) );
+				foreach ( T item in array )
+				{
+					if ( otherArray.Contains( item ) )
+						builder.Add( item );
+				}
+
+				return
+					builder.Count == builder.Capacity
+					? builder.MoveToImmutable()
+					: builder.ToImmutable();
+			}
+		}
+	}
+}

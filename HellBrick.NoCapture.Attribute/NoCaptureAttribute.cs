0235b3d26ee28bdf5efda6963ffda8d3b8a15512
HellBrick <hellbrick@outlook.com>
2018-03-18 12:47:10 +0300

    Added the project that contains [NoCapture] attribute

---
+++ HellBrick.NoCapture.Attribute/NoCaptureAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HellBrick
+{
+	[AttributeUsage( AttributeTargets.Parameter | AttributeTargets.Method, Inherited = false, AllowMultiple = false )]
+	public sealed class NoCaptureAttribute : Attribute
+	{
+	}
+}
